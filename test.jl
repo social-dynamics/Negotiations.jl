d5be09a7da90d2fc76f18f54e1910b02faca63cc
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2021-10-06 14:15:37 +0200

    read data from data folder, ignore data in repo

--- test.jl
+++ test.jl
@@ -3,13 +3,11 @@ using DataFrames
 using Clustering
 using UnicodePlots
 
-data = CSV.read(joinpath("..", "data-exploration", "data_wide.csv"), DataFrame)
+data = CSV.read(joinpath("data", "data_wide.csv"), DataFrame)
 m = Matrix(data[:, 3:end])
 clus = hclust(m)
 m_ordered = m[clus.order, :]
-
 m_dist = zeros(nrow(data), nrow(data))
-
 for i in 1:nrow(data)
     for j in 1:nrow(data)
         m_dist[i, j] = hamming(m[i, :], m[j, :])
