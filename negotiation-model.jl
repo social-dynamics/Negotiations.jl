3bb4932c4a8522675e0fda6a7a86fd35744304fc
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2021-10-11 22:35:10 +0200

    improved the model logic, implemented classic axelrod

--- negotiation-model.jl
+++ negotiation-model.jl
@@ -8,26 +8,31 @@ using DataFrames
 # The agent type
 @agent Negotiator{} Agents.GraphAgent begin
     opinions::AbstractArray
+    party::String
 end
 
-# Actions that each agent performs in each step
-function agent_step!(agent::Negotiator, model::Agents.ABM)
-    assimilate!(agent, model)
-    return model
+# Dummy function for agent step, the real stuff happens in model_step!
+function agent_step!(agent, model)
+    return agent
 end
 
 # Axelrod rule
-function assimilate!(agent, model)
-    interaction_partner = StatsBase.sample(nearby_ids(agent.pos, model, 1), 1)
-    if Random.rand() < 0.5  # TO DO: implement homophily
-        dim_to_switch = 1  # TO DO: choose at random
-        agent.opinions[1] = 1  # TO DO: implement ordinal Axelrod
+function assimilate!(agent1, agent2)
+    similarity = sum(agent1.opinions .== agent2.opinions) / length(agent1.opinions)
+    if Random.rand() < similarity
+        dim_to_switch = Random.rand(1:length(agent1.opinions))  # TO DO: choose at random
+        agent1.opinions[dim_to_switch] = copy(agent2.opinions[dim_to_switch])  # TO DO: implement ordinal Axelrod
     end
-    return agent
+    return agent1, agent2
 end
 
 # Run once every step to update model parameters
 function model_step!(model::Agents.ABM)
+    for i in 1:100000
+        agent1 = random_agent(model)
+        agent2 = random_agent(model)
+        assimilate!(agent1, agent2)
+    end
     return model
 end
 
@@ -40,32 +45,44 @@ function populate!(model::Agents.ABM, negotiator_group::AbstractArray)
     return model
 end
 
-function negotiation(parties::AbstractArray)
-    n_participants = length(parties)
-    space = Agents.GraphSpace(LightGraphs.complete_graph(n_participants * GROUPSIZE))
+# The "meta" model step: a selection of parties negotiate
+function negotiation(negotiators, parties::AbstractArray)
+    participants = filter(negotiator -> negotiator.party in parties, negotiators)
+    n_participants = length(participants)
+    space = Agents.GraphSpace(LightGraphs.complete_graph(n_participants))
     model = Agents.ABM(Negotiator, space)
-    populate!(model, parties)
-    adata, mdata = run!(model, agent_step!, model_step!, 100, adata=[:opinions], obtainer=deepcopy)
+    populate!(model, participants)
+    adata, mdata = run!(model, agent_step!, model_step!, 1, adata=[:opinions, :party], obtainer=deepcopy)
     return adata
 end
-
-# Setup (TO DO: wrap into functions)
-GROUPSIZE = 10
-space = Agents.GraphSpace(LightGraphs.complete_graph(GROUPSIZE))
-model = Agents.ABM(Negotiator, space)
 
 # TO DO: use actual data to initialize agents
 data = CSV.read(joinpath("data", "data_wide.csv"), DataFrame)
 
 # Setup negotiator groups
-spd = [Negotiator(i, i, zeros(Int, 38)) for i in 1:GROUPSIZE]
-cdu = [Negotiator(i, i, zeros(Int, 38)) for i in 11:GROUPSIZE * 2]
-gruene = [Negotiator(i, i, zeros(Int, 38)) for i in 21:GROUPSIZE * 3]
-fdp = [Negotiator(i, i, zeros(Int, 38)) for i in 31:GROUPSIZE * 4]
-afd = [Negotiator(i, i, zeros(Int,  38)) for i in 41:GROUPSIZE * 5]
-linke = [Negotiator(i, i, zeros(Int, 38)) for i in 51:GROUPSIZE * 6]
-ssw = [Negotiator(i, i, zeros(Int, 38)) for i in 61:GROUPSIZE * 7]
+party_names = ["spd", "cdu", "gruene", "fdp", "afd", "linke", "ssw"]
+GROUPSIZE = 10
+n_agents = length(party_names) * GROUPSIZE
+party_id = 1
+negotiators = []
+for party_id in 1:length(party_names)
+    for j in 1:GROUPSIZE
+        push!(negotiators,
+              Negotiator((party_id - 1) * GROUPSIZE + j, 
+                         party_id * j, 
+                         [Random.rand(1:10) for i in 1:38],
+                         party_names[party_id]))
+    end
+end
 
+# Test
+# negotiation(negotiators, ["gruene", "fdp"])
+
+
+# TODO:
+#   * initialize agents with actual wahlomat data
+#   * wrap initialization in function(s)
+#   * write a meta run! function that tracks the changes in the negotiator list
 
 
 
