a607f1741f1de6f56b2cfddbe7cd7d11f97dd211
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2021-10-20 12:31:28 +0200

    Refactored negotiation model
    Patrick and Johannes

--- negotiation-model.jl
+++ negotiation-model.jl
@@ -6,66 +6,23 @@ using CSV
 using DataFrames
 using Combinatorics
 
+ALL_SEATS = 736
+MAJORITY_REQUIREMENT = (736 / 2) + 1
+
+seats = Dict(
+    "SPD" => 206,
+    "GRUENE" => 118,
+    "FDP" => 92,
+    "CDU_CSU" => 197,
+    "AfD" => 83,
+    "SSW" => 1,
+    "LINKE" => 39
+)
+
 # The agent type
 @agent Negotiator{} Agents.GraphAgent begin
     opinions::AbstractArray
     party::String
-end
-
-# Dummy function for agent step, the real stuff happens in model_step!
-function agent_step!(agent, model)
-    return agent
-end
-
-# Ordinal similarity
-function similarity(agents)
-    sum(abs.(agents[1].opinions .- agents[2].opinions)) / (2 * length(agents[1].opinions))
-end
-
-# Axelrod rule
-function assimilate!(agent1, agent2)
-    i = Random.rand(1:length(agent1.opinions))
-    agent1.opinions[i] = sign(agent1.opinions[i] + agent2.opinions[i])
-    return agent1, agent2
-end
-
-# Run once every step to update model parameters
-function model_step!(model::Agents.ABM)
-    for i in 1:100000  # convergence criterium
-        agent1 = random_agent(model)
-        agent2 = random_agent(model)
-        if Random.rand() < similarity([agent1, agent2])
-            assimilate!(agent1, agent2)
-        end
-    end
-    return model
-end
-
-# Populate a model with agents
-function populate!(model::Agents.ABM, negotiator_group::AbstractArray)
-    negotiator_group = vcat(negotiator_group...)
-    for i in 1:nv(model.space.graph)
-        add_agent!(negotiator_group[i], i, model)
-    end
-    return model
-end
-
-# The "meta" model step: a selection of parties negotiate
-function negotiation!(negotiators, parties::AbstractArray)
-    participants = filter(negotiator -> negotiator.party in parties, negotiators)
-    n_participants = length(participants)
-    space = Agents.GraphSpace(LightGraphs.complete_graph(n_participants))
-    model = Agents.ABM(Negotiator, space)
-    populate!(model, participants)
-    adata, mdata = run!(model, agent_step!, model_step!, 1, adata=[:opinions, :party], obtainer=deepcopy)
-    return negotiators
-end
-
-# Extract opinion vectors from the formatted wahlomat data
-function get_party_opinions(party_name, data)
-    opinions = filter(data -> data.party_shorthand == party_name, data)
-    opinions = collect(opinions[1, 3:40])
-    return opinions
 end
 
 # Setup negotiator groups
@@ -76,7 +33,7 @@ function setup_negotiators(groupsize, party_names, data)
     for party_id in 1:length(party_names)
         curr_party_opinions = get_party_opinions(party_names[party_id], data)
         for j in 1:groupsize
-            agent = Negotiator((party_id - 1) * groupsize + j, 
+            agent = Negotiator((party_id - 1) * groupsize + j,
                                party_id * j,
                                deepcopy(curr_party_opinions),
                                party_names[party_id])
@@ -84,6 +41,13 @@ function setup_negotiators(groupsize, party_names, data)
         end
     end
     return negotiators
+end
+
+# Extract opinion vectors from the formatted wahlomat data
+function get_party_opinions(party_name, data)
+    opinions = filter(data -> data.party_shorthand == party_name, data)
+    opinions = collect(opinions[1, 3:40])
+    return opinions
 end
 
 # Get all n-combinations of the parties
@@ -107,22 +71,58 @@ function meta_run!(negotiators, party_combinations)
     return results
 end
 
-# Use actual data to initialize agents
-party_names_all = ["SPD", "CDU_CSU", "GRUENE", "FDP", "AfD", "DIE_LINKE", "SSW"]
-party_names = ["SPD", "CDU_CSU", "GRUENE", "FDP"]
-data = CSV.read(joinpath("data", "data_wide.csv"), DataFrame)
-data = filter(data -> data.party_shorthand in party_names, data)
+# The "meta" model step: a selection of parties negotiate
+function negotiation!(negotiators, parties::AbstractArray)
+    participants = filter(negotiator -> negotiator.party in parties, negotiators)
+    n_participants = length(participants)
+    space = Agents.GraphSpace(LightGraphs.complete_graph(n_participants))
+    model = Agents.ABM(Negotiator, space)
+    populate!(model, participants)
+    adata, mdata = run!(model, agent_step!, model_step!, 1, adata=[:opinions, :party], obtainer=deepcopy)
+    return negotiators
+end
 
-# Test
-negotiators = setup_negotiators(1, party_names, data)
-combs = get_party_combinations(party_names, 2)
-res = meta_run!(negotiators, combs)
+# Populate a model with agents
+function populate!(model::Agents.ABM, negotiator_group::AbstractArray)
+    negotiator_group = vcat(negotiator_group...)
+    for i in 1:nv(model.space.graph)
+        add_agent!(negotiator_group[i], i, model)
+    end
+    return model
+end
 
-# TODO:
-#   * refactor
-#   * aim at minimal coalition
-#       * seats in Bundestag as "currency"
-#   * add stubbornness (only move by one in every dimension)
+# Dummy function for agent step, the real stuff happens in model_step!
+function agent_step!(agent, model)
+    return agent
+end
+
+# Run once every step to update model parameters
+function model_step!(model::Agents.ABM)
+    for i in 1:100000  # convergence criterium
+        agent1 = random_agent(model)
+        agent2 = random_agent(model)
+        if Random.rand() < similarity([agent1, agent2])
+            assimilate!(agent1, agent2)
+        end
+    end
+    return model
+end
+
+# Axelrod rule
+function assimilate!(agent1, agent2)
+    i = Random.rand(1:length(agent1.opinions))
+    agent1.opinions[i] = sign(agent1.opinions[i] + agent2.opinions[i])
+    return agent1, agent2
+end
+
+# Ordinal similarity
+function similarity(agents::Array{Negotiator})  # TODO: refactor
+    1 - (sum(abs.(agents[1].opinions .- agents[2].opinions)) / (2 * length(agents[1].opinions)))
+end
+
+function similarity(opinions::AbstractArray)
+    1 - (sum(abs.(opinions[1] .- opinions[2])) / (2 * length(opinions[1])))
+end
 
 # TODO: fix bug
 function can_form_government(parties, negotiators, seats)
@@ -133,21 +133,41 @@ function can_form_government(parties, negotiators, seats)
             sum_seats += v
         end
     end
-    has_majority = sum_seats >= MAJORITY_REQUIREMENT ? true : false
-    combs = collect(combinations(candidates, 2))
-    has_consensus = (sum(similarity.(combs))) / length(combs)) == 1.
+    has_majority = sum_seats >= MAJORITY_REQUIREMENT ? true : false  # check
+    # combs = collect(combinations(candidates, 2))
+    # has_consensus = (sum(similarity.(combs)) / length(combs)) == 1.
+    party_consensus_opinions = [party_consensus(negotiators, p) for p in parties]
+    sims = [similarity(o) for o in collect(combinations(party_consensus_opinions, 2))]
+    has_consensus = reduce(&, sims .> 0.9)
     return has_majority & has_consensus
 end
 
-ALL_SEATS = 736
-MAJORITY_REQUIREMENT = (736 / 2) + 1
+function party_consensus(negotiators, party)
+    party_negotiators = filter(negotiators -> negotiators.party == party, negotiators)
+    negotiator_opinions = [pn.opinions for pn in party_negotiators]
+    party_consensus_opinions = [StatsBase.mode([o[i] for o in negotiator_opinions]) for i in 1:length(negotiator_opinions[1])]
+    return party_consensus_opinions
+end
 
-seats = Dict(
-    "SPD" => 206,
-    "GRUENE" => 118,
-    "FDP" => 92,
-    "CDU_CSU" => 197,
-    "AfD" => 83,
-    "SSW" => 1,
-    "LINKE" => 39
-)
+# get party consensus
+# compute similarity of all involved parties
+# test if > threshold (simplest: 1 => completely equal)
+
+
+# Use actual data to initialize agents
+party_names_all = ["SPD", "CDU_CSU", "GRUENE", "FDP", "AfD", "DIE_LINKE", "SSW"]
+party_names = ["SPD", "CDU_CSU", "GRUENE", "FDP"]
+data = CSV.read(joinpath("data", "data_wide.csv"), DataFrame)
+data = filter(data -> data.party_shorthand in party_names, data)
+
+# Test
+negotiators = setup_negotiators(10, party_names, data)
+combs = get_party_combinations(party_names, 2)
+res = meta_run!(negotiators, combs)
+
+# TODO:
+#   * refactor
+#   * aim at minimal coalition
+#       * seats in Bundestag as "currency"
+#   * add stubbornness (only move by one in every dimension)
+
