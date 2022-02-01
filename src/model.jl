97b79c52c01dcbdb3eec4d1d60403beb2cd994ed
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-02-01 12:18:49 +0100

    Refactored agent creation
    * the process is now simplified
    * negotiation sequence not part of the model anymore

--- src/model.jl
+++ src/model.jl
@@ -18,40 +18,47 @@ The model type for the party negotiation model.
 mutable struct Model
     parameter_set::ParameterSet
     agents::Array{Agent}
-    negotiation_sequence::Vector{Vector{String}}
 end
 
 
 """
-    setup_model(params::ParameterSet, negotiation_sequence::Vector{Vector{String})
+    setup_model(params::ParameterSet, db::SQLite.DB)
 
-Create a model from a given set of specifications.
+Create a model from a given set of specifications and a database.
 """
-function setup_model(
-    params::ParameterSet,
-    negotiation_sequence::Vector{Vector{String}},
-    db::SQLite.DB
-)
+function setup_model(params::ParameterSet, db::SQLite.DB)
     agents = create_agents(params, db)
-    return Model(params, agents, negotiation_sequence)
+    return Model(params, agents)
 end
 
 
-# TODO: currently broken due to updated config procedure
-# TODO: refactor, this can be simplified
 """
     create_agents(params::ParameterSet)
 
 Create a vector of agents with the specifications from a `ParameterSet`.
 """
 function create_agents(params::ParameterSet, db::SQLite.DB)
-    n_agents = length(params.parties) * params.group_size
-    agent_ids = 1:n_agents
-    agent_parties = reduce(
-        vcat,
-        [repeat([party], params.group_size) for party in params.parties]
+    agent_parties = reduce(vcat, fill.(params.parties, params.group_size))
+    opinions = Dict(
+        party => filter(p -> p.party_shorthand == party, party_opinions_view(db)).position
+        for party in params.parties
     )
-    opinions_with_party = DBInterface.execute(
+    agent_opinions = [opinions[party] for party in agent_parties]
+    agents = [
+        Agent(i, p, deepcopy(o))
+        for (i, (p, o)) in enumerate(zip(agent_parties, agent_opinions))
+    ]
+    return agents
+end
+
+
+"""
+    party_opinions_view(db::SQLite.DB)
+
+Return a dataframe with party, statement and held position on that statement.
+"""
+function party_opinions_view(db::SQLite.DB)
+    return DBInterface.execute(
         db,
         """
         SELECT party_shorthand, statement_id, position
@@ -59,19 +66,6 @@ function create_agents(params::ParameterSet, db::SQLite.DB)
         ON opinion.party_id = party.party_id
         """
     ) |> DataFrame
-    opinions = Dict(
-        party => filter(
-            p -> p.party_shorthand == party,
-            opinions_with_party
-           ).position
-        for party in params.parties
-    )
-    agent_opinions = [opinions[party] for party in agent_parties]
-    agents = [
-        Agent(i, p, deepcopy(o))
-        for (i, p, o) in zip(agent_ids, agent_parties, agent_opinions)
-    ]
-    return agents
 end
 
 
--- test.jl
+++ test.jl
@@ -3,6 +3,8 @@ Pkg.activate(".")
 
 using Negotiations
 
-params, db = read_config("config.yaml")
-a = Negotiations.create_agents(params, db)
-print(a)
+
+params = parameter_set_from_config("test/test-config.yaml")
+include("test/create-db.jl")
+db = load_database("test.sqlite")
+
